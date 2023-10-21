8a82531d523c8cb30d01f8413ef9fcc14965ee17
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-10-21 20:29:56 +0200

    Aktualisiere pxt.json, main.blocks, i2c.ts, i2cscan.ts

--- i2cscan.ts
+++ i2cscan.ts
@@ -3,14 +3,13 @@ namespace i2c
 /*
 */ {
 
-    //% group="i2c Adressen" subcategory="i2c Scan"
-    //% block="i2c-Check von %vonADDR bis %bisADDR Pause %ms ms" weight=2
+    //% group="sammelt gültige i2c-Adressen in Array" subcategory="i2c Scan"
+    //% block="i2c-Scan von %vonADDR bis %bisADDR Pause %ms ms"
     //% vonADDR.shadow="i2c_eADDR" bisADDR.shadow="i2c_eADDR"
     //% vonADDR.min=0 vonADDR.max=127
     //% bisADDR.min=0 bisADDR.max=127 bisADDR.defl=i2c.eADDR.LCD_20x4_x72
     //% ms.min=0 ms.max=500 ms.defl=100
-    export function i2cscan(vonADDR: number, bisADDR: number, ms: number) {
-        //return i2cCheck(vonADDR, bisADDR) 
+    export function i2cscan(vonADDR: number, bisADDR: number, ms: number): number[] {
         let a: number[] = []
         if (between(vonADDR, 0, 127) && between(bisADDR, 0, 127) && vonADDR <= bisADDR) {
             let b = Buffer.create(1)
@@ -29,6 +28,26 @@ namespace i2c
         return a
     }
 
-    function between(i0: number, i1: number, i2: number): boolean { return (i0 >= i1 && i0 <= i2) }
+
+    //% group="sammelt gültige i2c-Adressen in Array" subcategory="i2c Scan"
+    //% block="Array %bytes toHex()"
+    export function arrayToHex(bytes: number[]): string {
+        return Buffer.fromArray(bytes).toHex()
+    }
+
+    //% group="sammelt gültige i2c-Adressen in HEX-String" subcategory="i2c Scan"
+    //% block="i2c-Scan von %vonADDR bis %bisADDR Pause %ms ms"
+    //% vonADDR.shadow="i2c_eADDR" bisADDR.shadow="i2c_eADDR"
+    //% vonADDR.min=0 vonADDR.max=127
+    //% bisADDR.min=0 bisADDR.max=127 bisADDR.defl=i2c.eADDR.LCD_20x4_x72
+    //% ms.min=0 ms.max=500 ms.defl=100
+    export function i2ctohex(vonADDR: number, bisADDR: number, ms: number): string {
+        return Buffer.fromArray(i2cscan(vonADDR, bisADDR, ms)).toHex()
+    }
+
+
+    //% group="Logik" subcategory="i2c Scan"
+    //% block="%i0 zwischen %i1 und %i2" weight=4
+    export function between(i0: number, i1: number, i2: number): boolean { return (i0 >= i1 && i0 <= i2) }
 
 } // i2cscan.ts
