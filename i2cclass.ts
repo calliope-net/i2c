6ac1d0508c412238866095e69a9a3936113bee75
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-11-04 20:59:59 +0100

    Aktualisiere i2cclass.ts

--- i2cclass.ts
+++ i2cclass.ts
@@ -8,18 +8,56 @@ namespace i2c {
     export function fromArray1(bytes: number[]) { return new i2cclass(Buffer.fromArray(bytes)) }
 
 
+
+    // ========== subcategory="Buffer.create" ==========
+
+    // ========== group="Buffer anlegen" subcategory="Buffer.create"
+
     //% group="Buffer anlegen" subcategory="Buffer.create"
     //% block="Buffer.create size %size" weight=8
     //% blockSetVariable=oBuffer
     export function createClass(size: number): i2cclass { return new i2cclass(Buffer.create(size)) }
+
+    // ========== setUint8 weight=6
+    // ========== setNumber weight=4
+    // ========== length weight=2
+
+    //% group="Buffer anlegen" subcategory="Buffer.create"
+    //% block="sizeOfNumberFormat %format" weight=1
+    //% format.defl=NumberFormat.UInt8LE
+    export function osizeOfNumberFormat(format: NumberFormat): 0 | 4 | 2 | 1 | 8 { return Buffer.sizeOfNumberFormat(format) }
+
+
+
+
+    // ========== group="Buffer anlegen aus Daten" subcategory="Buffer.create"
 
     //% group="Buffer anlegen aus Daten" subcategory="Buffer.create"
     //% block="Buffer.fromArray (%bytes)" weight=8
     //% blockSetVariable=oBuffer
     export function fromArray2(bytes: number[]) { return new i2cclass(Buffer.fromArray(bytes)) }
 
+    //% group="Buffer anlegen aus Daten" subcategory="Buffer.create"
+    //% block="Buffer.fromString(%str)" weight=6
+    //% blockSetVariable=oBuffer
+    export function ofromUTF8(str: string) { return new i2cclass(Buffer.fromUTF8(str)) }
 
-    // ========== class i2cclass
+    //% group="Buffer anlegen aus Daten" subcategory="Buffer.create"
+    //% block="Buffer.fromHex(%hex)" weight=4
+    //% blockSetVariable=oBuffer
+    export function ofromHex(hex: string) { return new i2cclass(Buffer.fromHex(hex)) }
+
+    // ========== oBuffer.concat  weight=3
+
+    //% group="Buffer anlegen aus Daten" subcategory="Buffer.create"
+    //% block="Buffer %pBuffer" weight=2
+    //% blockSetVariable=oBuffer
+    export function fromBuffer(pBuffer: Buffer) { return new i2cclass(pBuffer) }
+
+
+    // ====================================
+    // ========== class i2cclass ==========
+
     export class i2cclass {
         qBuffer: Buffer
 
@@ -28,7 +66,6 @@ namespace i2c {
         }
 
         // ========== group="i2c Buffer senden an Modul"
-
 
         //% group="Buffer senden an i2c-Modul"
         //% block="writeBuffer %oBuffer → i2c %pADDR || repeat %repeat" weight=6
@@ -45,13 +82,14 @@ namespace i2c {
 
 
 
+        // ========== subcategory="Buffer.create" ==========
 
+        // ========== group="Buffer anlegen" subcategory="Buffer.create"
 
         //% group="Buffer anlegen" subcategory="Buffer.create"
         //% block="Buffer %oBuffer .setUint8(offset %off byte %byte)" weight=6
         //% byte.min=0 byte.max=255
         setUint8(off: number, byte: number) { this.qBuffer.setUint8(off, byte) }
-
 
         //% group="Buffer anlegen" subcategory="Buffer.create"
         //% block="Buffer %oBuffer .setNumber(%format offset %off value %value)" weight=4
@@ -60,15 +98,11 @@ namespace i2c {
         setNumber(format: NumberFormat, off: number, value: number) { this.qBuffer.setNumber(format, off, value) }
 
         //% group="Buffer anlegen" subcategory="Buffer.create"
-        //% block="sizeOfNumberFormat %format" weight=2
-        //% format.defl=NumberFormat.UInt8LE
-        //sizeOfNumberFormat(format: NumberFormat): 0 | 4 | 2 | 1 | 8 { return Buffer.sizeOfNumberFormat(format) }
-
-        //% group="Buffer anlegen" subcategory="Buffer.create"
         //% block="Buffer %oBuffer .length" weight=2
         length(): number { return this.qBuffer.length }
 
 
+        // ========== group="Buffer anlegen aus Daten" subcategory="Buffer.create"
 
         //% group="Buffer anlegen aus Daten" subcategory="Buffer.create"
         //% block="Buffer %oBuffer .concat(otherBuffer %other)" weight=3
@@ -78,9 +112,11 @@ namespace i2c {
 
 
 
-    } // ========== class i2cclass
+    } // ========== class i2cclass ========
+    // ====================================
 
-    // namespace i2c
+
+    // ========== namespace i2c ==========
 
     // ========== group="Buffer empfangen von i2c-Modul"
 
@@ -89,35 +125,6 @@ namespace i2c {
     //% pADDR.shadow="i2c_eADDR"
     //% blockSetVariable=oBuffer
     export function oi2cReadBuffer(pADDR: number, size: number, repeat: boolean = false) { return new i2cclass(pins.i2cReadBuffer(pADDR, size, repeat)) }
-
-
-    //% group="Buffer anlegen" subcategory="Buffer.create"
-    //% block="sizeOfNumberFormat %format" weight=1
-    //% format.defl=NumberFormat.UInt8LE
-    export function osizeOfNumberFormat(format: NumberFormat): 0 | 4 | 2 | 1 | 8 { return Buffer.sizeOfNumberFormat(format) }
-
-
-    // ========== group="Buffer anlegen aus Daten" subcategory="Buffer.create"
-
-
-
-    //% group="Buffer anlegen aus Daten" subcategory="Buffer.create"
-    //% block="Buffer.fromString(%str)" weight=5
-    //% blockSetVariable=oBuffer
-    export function ofromUTF8(str: string) { return new i2cclass(Buffer.fromUTF8(str)) }
-
-    //% group="Buffer anlegen aus Daten" subcategory="Buffer.create"
-    //% block="Buffer.fromHex(%hex)" weight=4
-    //% blockSetVariable=oBuffer
-    export function ofromHex(hex: string) { return new i2cclass(Buffer.fromHex(hex)) }
-
-
-    // ========== oBuffer.concat  weight=3
-
-    //% group="Buffer anlegen aus Daten" subcategory="Buffer.create"
-    //% block="Buffer %pBuffer" weight=1
-    //% blockSetVariable=oBuffer
-    export function fromBuffer(pBuffer: Buffer) { return new i2cclass(pBuffer) }
 
 
 
